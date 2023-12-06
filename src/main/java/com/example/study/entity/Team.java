1bb0cec998b24d04ac0aa924b42739a0bb687cad
yoojin <yoojin9199@gmail.com>
2023-12-06 12:42:34 +0900

    #1. 실행 환경 및 빌드 구성

---
+++ src/main/java/com/example/study/entity/Team.java
@@ -0,0 +1,26 @@
+package com.example.study.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@ToString(exclude = "members") @EqualsAndHashCode(of = "id")
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_team")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}
